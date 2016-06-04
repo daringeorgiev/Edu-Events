9131a19c73a457968a09cc400e8a5aede104df1a
Svilko <svilkors@gmail.com>
2016-06-04 13:04:07 +0300

    added routes for register, edit and view events

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,25 @@
-var app = angular.module('eventApp',['ngRoute']);
+var app = angular.module('eventApp',['ngRoute'])
 
+.config(['$routeProvider',function($routeProvider){
+        'use strict';
+    $routeProvider
+        .when('/',{
+            reloadOnSearch: false
+        })
+        .when('/register', {
+            template: '<div>register</div>',
+            controller: '',
+            reloadOnSearch: false
+        })
+        .when('/edit-event', {
+            template: '<div>edit event</div>',
+            controller: '',
+            reloadOnSearch: false
+        })
+        .when('/view-events', {
+            template: '<div>view events</div>',
+            controller: '',
+            reloadOnSearch: false
+        })
+        .otherwise({redirectTo: '/'})
+}]);
