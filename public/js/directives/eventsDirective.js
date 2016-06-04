bea476ecd1bc59c68f664b1336dead3373a3512e
huuuskyyy <huuuskyyy@pismo.bg>
2016-06-04 22:00:20 +0300

    Added event directive and partial template

---
+++ public/js/directives/eventsDirective.js
@@ -0,0 +1,10 @@
+﻿app.directive('event', function () {
+    return {
+        restrict: 'AEC',
+        replace: true,
+        templateUrl: '../partials/event-item.html',
+        scope:{
+            event: '=eventData'
+        }
+    };
+});
